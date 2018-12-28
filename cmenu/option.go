50655ee7cdadf3f243ef537c50337accffd8d173
Coleman Word <coleman@Colemans-MacBook-Pro.local>
2018-12-28 15:48:49 -0700

    added cmenu

---
+++ cmenu/option.go
@@ -0,0 +1,20 @@
+package cmenu
+
+
+type Opt struct {
+	ID        int
+	Text      string
+	Value     interface{}
+	function  func(Opt) error
+	isDefault bool
+}
+
+func newOption(id int, text string, value interface{}, def bool, function func(Opt) error) *Opt {
+	return &Opt{
+		ID:        id,
+		Text:      text,
+		Value:     value,
+		isDefault: def,
+		function:  function,
+	}
+}
