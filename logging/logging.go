84a39249dc4924b8df5c98445ce15247423447f7
Coleman Word <coleman@Colemans-MacBook-Pro.local>
2018-12-28 14:55:07 -0700

    added wlog & gokit log

--- logging/logging.go
+++ logging/logging.go
@@ -49,8 +49,6 @@ func (l *Logger) FatalUI(args ...interface{}) {
 			l.UI.Log(strconv.FormatBool(x))
 		case byte:
 			l.UI.Log(string(x))
-		default:
-			log.Fatalf("%s", "fatal: ", args)
 		}
 	}
 }
@@ -68,9 +66,6 @@ func (l *Logger) InfoUI(args ...interface{}) {
 			l.UI.Info(strconv.FormatBool(x))
 		case byte:
 			l.UI.Info(string(x))
-
-		default:
-			log.Printf("%s", "info: ", args)
 		}
 	}
 }
@@ -88,8 +83,6 @@ func (l *Logger) ErrorUI(args ...interface{}) {
 			l.UI.Error(strconv.FormatBool(x))
 		case byte:
 			l.UI.Error(string(x))
-		default:
-			log.Fatalf("%s", "error: ", args)
 		}
 	}
 }
@@ -107,8 +100,6 @@ func (l *Logger) OutputUI(args ...interface{}) {
 			l.UI.Output(strconv.FormatBool(x))
 		case byte:
 			l.UI.Output(string(x))
-		default:
-			log.Printf("%s", "output: ", args)
 		}
 	}
 }
@@ -126,8 +117,6 @@ func (l *Logger) RunningUI(args ...interface{}) {
 			l.UI.Running(strconv.FormatBool(x))
 		case byte:
 			l.UI.Running(string(x))
-		default:
-			log.Printf("%s", "running: ", args)
 		}
 	}
 }
@@ -146,8 +135,6 @@ func (l *Logger) WarnUI(args ...interface{}) {
 			l.UI.Warn(strconv.FormatBool(x))
 		case byte:
 			l.UI.Warn(string(x))
-		default:
-			log.Printf("%s", "warning: ", args)
 		}
 	}
 }
@@ -165,8 +152,6 @@ func (l *Logger) SuccessUI(args ...interface{}) {
 			l.UI.Success(strconv.FormatBool(x))
 		case byte:
 			l.UI.Success(string(x))
-		default:
-			log.Printf("%s", "success: ", args)
 		}
 	}
 }
@@ -185,8 +170,6 @@ func (l *Logger) DebugUI(args ...interface{}) {
 			l.UI.Info(strconv.FormatBool(x))
 		case byte:
 			l.UI.Info(string(x))
-		default:
-			log.Printf("%s", "debug: ", args)
 		}
 	}
 }
