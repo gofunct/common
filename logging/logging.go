482630d80f2347d8f8cc13aa185966de76fa0c3d
Coleman Word <coleman@Colemans-MacBook-Pro.local>
2018-12-28 15:07:41 -0700

    added wlog & gokit log

--- logging/logging.go
+++ logging/logging.go
@@ -6,12 +6,15 @@ import (
 	kitlog "github.com/go-kit/kit/log"
 	"log"
 	"os"
-	"strconv"
 )
 
 type Logger struct{
 	UI 		wlog.UI
 	KitLog kitlog.Logger
+}
+
+func init() {
+	log.SetOutput(kitlog.NewStdlibAdapter(l.KitLog))
 }
 
 func NewLogger() *Logger {
@@ -33,143 +36,4 @@ func NewLogger() *Logger {
 
 	log.SetOutput(kitlog.NewStdlibAdapter(l.KitLog))
 	return l
-}
-
-
-func (l *Logger) FatalUI(args ...interface{}) {
-	for _, arg := range args {
-		switch x := arg.(type) {
-		case string:
-			l.UI.Log(x)
-		case error:
-			l.UI.Log(x.Error())
-		case int:
-			l.UI.Log(string(x))
-		case bool:
-			l.UI.Log(strconv.FormatBool(x))
-		case byte:
-			l.UI.Log(string(x))
-		}
-	}
-}
-
-func (l *Logger) InfoUI(args ...interface{}) {
-	for _, arg := range args {
-		switch x := arg.(type) {
-		case string:
-			l.UI.Info(x)
-		case error:
-			l.UI.Info(x.Error())
-		case int:
-			l.UI.Info(string(x))
-		case bool:
-			l.UI.Info(strconv.FormatBool(x))
-		case byte:
-			l.UI.Info(string(x))
-		}
-	}
-}
-
-func (l *Logger) ErrorUI(args ...interface{}) {
-	for _, arg := range args {
-		switch x := arg.(type) {
-		case string:
-			l.UI.Error(x)
-		case error:
-			l.UI.Error(x.Error())
-		case int:
-			l.UI.Error(string(x))
-		case bool:
-			l.UI.Error(strconv.FormatBool(x))
-		case byte:
-			l.UI.Error(string(x))
-		}
-	}
-}
-
-func (l *Logger) OutputUI(args ...interface{}) {
-	for _, arg := range args {
-		switch x := arg.(type) {
-		case string:
-			l.UI.Output(x)
-		case error:
-			l.UI.Output(x.Error())
-		case int:
-			l.UI.Output(string(x))
-		case bool:
-			l.UI.Output(strconv.FormatBool(x))
-		case byte:
-			l.UI.Output(string(x))
-		}
-	}
-}
-
-func (l *Logger) RunningUI(args ...interface{}) {
-	for _, arg := range args {
-		switch x := arg.(type) {
-		case string:
-			l.UI.Running(x)
-		case error:
-			l.UI.Running(x.Error())
-		case int:
-			l.UI.Running(string(x))
-		case bool:
-			l.UI.Running(strconv.FormatBool(x))
-		case byte:
-			l.UI.Running(string(x))
-		}
-	}
-}
-
-
-func (l *Logger) WarnUI(args ...interface{}) {
-	for _, arg := range args {
-		switch x := arg.(type) {
-		case string:
-			l.UI.Warn(x)
-		case error:
-			l.UI.Warn(x.Error())
-		case int:
-			l.UI.Warn(string(x))
-		case bool:
-			l.UI.Warn(strconv.FormatBool(x))
-		case byte:
-			l.UI.Warn(string(x))
-		}
-	}
-}
-
-func (l *Logger) SuccessUI(args ...interface{}) {
-	for _, arg := range args {
-		switch x := arg.(type) {
-		case string:
-			l.UI.Success(x)
-		case error:
-			l.UI.Success(x.Error())
-		case int:
-			l.UI.Success(string(x))
-		case bool:
-			l.UI.Success(strconv.FormatBool(x))
-		case byte:
-			l.UI.Success(string(x))
-		}
-	}
-}
-
-
-func (l *Logger) DebugUI(args ...interface{}) {
-	for _, arg := range args {
-		switch x := arg.(type) {
-		case string:
-			l.UI.Info(x)
-		case error:
-			l.UI.Info(x.Error())
-		case int:
-			l.UI.Info(string(x))
-		case bool:
-			l.UI.Info(strconv.FormatBool(x))
-		case byte:
-			l.UI.Info(string(x))
-		}
-	}
 }
