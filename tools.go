c0fb40ab619ba9203c3c53a4fc7eba1554c9667b
Coleman Word <coleman@Colemans-MacBook-Pro.local>
2018-12-29 19:59:50 -0700

    deps

--- tools.go
+++ tools.go
@@ -1,10 +1,14 @@
 // +build tools
 
-package main
+package tools
 
 // tool dependencies
 import (
 	_ "github.com/haya14busa/reviewdog/cmd/reviewdog"
 	_ "github.com/srvc/wraperr/cmd/wraperr"
 	_ "golang.org/x/lint/golint"
+	- "github.com/kisielk/errcheck"
+	_ "github.com/mitchellh/gox"
+	_ "honnef.co/go/tools"
+	_ "mvdan.cc/unparam"
 )
