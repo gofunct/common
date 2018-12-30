d788422b7969f6dd077d18ad871f7ec786123db7
Coleman Word <coleman@Colemans-MacBook-Pro.local>
2018-12-29 21:21:22 -0700

    protoc

--- tools.go
+++ tools.go
@@ -4,11 +4,15 @@ package tools
 
 // tool dependencies
 import (
+	_ "github.com/golang/mock/mockgen"
+	_ "github.com/google/wire/cmd/wire"
 	_ "github.com/haya14busa/reviewdog/cmd/reviewdog"
+	_ "github.com/gofunct/bingen"
+	_ "github.com/jessevdk/go-assets-builder"
+	_ "github.com/kisielk/errcheck"
+	_ "github.com/mitchellh/gox"
 	_ "github.com/srvc/wraperr/cmd/wraperr"
 	_ "golang.org/x/lint/golint"
-	- "github.com/kisielk/errcheck"
-	_ "github.com/mitchellh/gox"
-	_ "honnef.co/go/tools"
+	_ "honnef.co/go/tools/cmd/megacheck"
 	_ "mvdan.cc/unparam"
 )
